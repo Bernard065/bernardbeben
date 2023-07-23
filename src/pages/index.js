daf3758036217ae239fd3a2b47a8844e720d5135
+ <+>
2023-07-24 00:39:31 +0300

    creating download and contact me links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
 import profilePic from '../../public/images/profile/developer-pic-1.png'
 import AnimatedText from '@/components/AnimatedText'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 export default function Home() {
   return (
@@ -23,9 +24,21 @@ export default function Home() {
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text='Transforming Imagination into Reality with Code and Design.' className='!text-6xl !text-left' />
-              <p>
-              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in web development. Let's bring digital vision to life through code!
-              </p>
+              <motion.p
+                initial={{ x: -200, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 1.5 }}
+                className='text-white text-lg mt-4 text-left'
+              >
+                As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in web development. Let's bring digital vision to life through code!
+              </motion.p>
+              <div className='flex items-center self-start mt-5'>
+                <Link href='/bernardResume.pdf' target={'-blank'} className='bg-gray-500 hover:bg-gray-700 text-white text-lg px-4 py-2 rounded-md mr-4'>
+                  <span className='mr-2'>Resume</span>
+                  <FileDownloadIcon className='h-5 w-5' />
+                </Link>
+                <Link href='bernardbebeni@gmail.com' target={'-blank'} className='ml-4 text-lg font-medium capitalize text-white underline hover:text-orange-500'>Contact Me</Link>
+            </div>
             </div>
           </div>
         </Layout>
