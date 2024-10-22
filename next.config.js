221b25d86a0d9c5296e6a69905d4718a65734f63
Bernard065 <bebenibernard@gmail.com>
2024-10-22 10:13:22 +0300

    Fixing IfNamespace error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    react: {
+      throwIfNamespace: false, // Disable JSX namespace error
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
